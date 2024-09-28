4409cec2c067197eabc4fa0aa240ebdf325332ab
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-09-28 17:35:39 +0500

    creating nested routes in next js

---
+++ app/posts/new/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+// this is page is rendered as /posts/new
+
+const page = () => {
+  return (
+    <div>
+      New Post  
+    </div>
+  )
+}
+
+export default page
---
+++ app/posts/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+// This page is rendered as /posts
+const page = () => {
+  return (
+    <div>
+      posts
+    </div>
+  )
+}
+
+export default page
