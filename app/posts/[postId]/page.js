9d5ef6542d2fb96d67ceb661bedbb6bda69a0d8f
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-10-03 23:50:11 +0500

    Update page.js
    Use the next-helmet or helmet package to set secure HTTP headers that protect your app from common vulnerabilities like clickjacking.

--- app/posts/[postId]/page.js
+++ app/posts/[postId]/page.js
@@ -23,3 +23,22 @@ export default function SanitizeInput({ userInput }) {
   );
 }
 
+import { Fragment } from 'react';
+import Helmet from 'helmet';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Fragment>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta http-equiv="Content-Security-Policy" content="default-src 'self';" />
+      </Helmet>
+      <Component {...pageProps} />
+    </Fragment>
+  );
+}
+
+export default MyApp;
+
