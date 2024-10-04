477a461cf07fe1c42b07630517bf536be4f61d34
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-10-04 23:42:56 +0500

    Update page.js
    remove extra files and folders

--- app/posts/[postId]/page.js
+++ app/posts/[postId]/page.js
@@ -12,33 +12,3 @@ export default page
 
 
 
-
-import DOMPurify from 'dompurify';
-
-export default function SanitizeInput({ userInput }) {
-  const cleanHtml = DOMPurify.sanitize(userInput);
-
-  return (
-    <div dangerouslySetInnerHTML={{ __html: cleanHtml }} />
-  );
-}
-
-import { Fragment } from 'react';
-import Helmet from 'helmet';
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <Fragment>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta http-equiv="Content-Security-Policy" content="default-src 'self';" />
-      </Helmet>
-      <Component {...pageProps} />
-    </Fragment>
-  );
-}
-
-export default MyApp;
-
