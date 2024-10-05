ce2ff54378ae2a35531db4cc94cb9f03b8055fc9
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-10-05 23:40:36 +0500

    Update page.js
    postId is a dynamic URL parameter, you can use the useRouter hook from Next.js to grab it from the URL

--- app/posts/[postId]/page.js
+++ app/posts/[postId]/page.js
@@ -12,3 +12,19 @@ export default page
 
 
 
+
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const Page = () => {
+  const router = useRouter();
+  const { postId } = router.query; // Fetches the postId from the URL
+
+  return (
+    <div>
+      {postId ? <h1>Post ID: {postId}</h1> : <p>Loading...</p>}
+    </div>
+  );
+};
+
+export default Page;
