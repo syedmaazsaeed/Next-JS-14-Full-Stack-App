bae7007db96f70fe96b93f04947ec3afa45ccb14
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-10-08 23:53:02 +0500

    Update page.js
    Remove Un necessary lines or content from code file

--- app/posts/[postId]/page.js
+++ app/posts/[postId]/page.js
@@ -10,21 +10,3 @@ const page = () => {
 
 export default page
 
-
-
-
-import React from 'react';
-import { useRouter } from 'next/router';
-
-const Page = () => {
-  const router = useRouter();
-  const { postId } = router.query; // Fetches the postId from the URL
-
-  return (
-    <div>
-      {postId ? <h1>Post ID: {postId}</h1> : <p>Loading...</p>}
-    </div>
-  );
-};
-
-export default Page;
